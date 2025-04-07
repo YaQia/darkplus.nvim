bbad733c29ad40cc497d97433d33271f7af8a807
姚益祁 <61108050+YaQia@users.noreply.github.com>
2025-04-07 23:58:42 +0800

    Update theme.lua for snacks.nvim indent

--- lua/darkplus/theme.lua
+++ lua/darkplus/theme.lua
@@ -413,7 +413,7 @@ theme.set_highlights = function()
   hl(0, "IndentBlanklineContextStart", { fg = 'NONE', bg = 'NONE', underline = true, })
   hl(0, "IndentBlanklineChar", { fg = c.dark_gray, bg = 'NONE' })
   -- snacks.nvim indent
-  hl(0, "SnacksIndentScope", { fg = '#f4f4f4', bg = 'NONE' })
+  hl(0, "SnacksIndentScope", { fg = c.gray, bg = 'NONE' })
   hl(0, "SnacksIndent", { fg = c.line, bg = 'NONE' })
   
   -- Dashboard
